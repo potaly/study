212a98a3cb7bfb9c6dd69fa1689a17a593faa1fc
yhb3420 <hongbo_yuan@foxmail.com>
2014-04-14 00:12:28 +0800

    oracle pivot

---
+++ oracle/pivot/pivot.sql
@@ -0,0 +1,23 @@
+select t.* from TEST_PIVOT t;
+--name  type  score
+--张三  语文  91
+--张三  数学  89
+--李四  语文  79
+--李四  数学  98
+
+--使用decode+group实现行转列
+select name,
+       max(decode(type, '语文', score, null)) as Chinese,
+       max(decode(type, '数学', score, null)) as math
+  from test_pivot
+ group by name;
+
+--使用pivot实现行转列
+select *
+  from (select name, type, score from test_pivot) 
+ pivot(max(score) 
+       for type 
+       in('语文' as Chinese,'数学' as math)
+ );
+
+ 
