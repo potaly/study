8aba914fa4d81f14abd1b6022c7c4c3c4edcf57d
yhb3420 <hongbo_yuan@foxmail.com>
2014-04-28 23:24:29 +0800

    replaceAll
    replace应用扩展

--- js/replaceAll/README.md
+++ js/replaceAll/README.md
@@ -1,5 +1,5 @@
 
-##JavaScript replace增强函数jQuick.replaceAll
+##JavaScript replace实现简单模板引擎
   
   
 	var demoData = {
@@ -16,27 +16,18 @@
 			}
 		}
 	}
-	var demoDataArray = [];
-	for ( var i = 10; i--;) {
-		demoDataArray.push(demoData);
-	}
   
 	function demo1() {
 		var demo1 = document.getElementById("demo1");
-		demo1.innerHTML = jQuick.replaceAll(demo1.innerHTML,demoData);
+		demo1.innerHTML = replaceAll(demo1.innerHTML,demoData);
 		demo1 = function(){alert('不要重复执行。')}
 	}
   
-	function demo2() {
-		var demo2 = document.getElementById("demo2");
-		demo2.innerHTML = jQuick.replaceAll(demo2.innerHTML,demoDataArray);
-		demo2 = function(){alert('不要重复执行。')}
-	}
 	
 	
 ##jsReplaceAll Object Demo
   
-###Demo1:
+###Demo:
   
 	<input type="button" value="run" onclick="demo1();" />
 	<div id="demo1">
@@ -51,19 +42,3 @@
 		</ul>
 	</div>
   
-##jsReplaceAll Array Demo
-  
-###Demo2:
-
-	<input type="button" value="run" onclick="demo2();" />
-	<div id="demo2">
-		<ul>
-			<li>index:${data.index}</li>
-			<li>intValue(数字):${data.intValue}</li>
-			<li>intValueAgain:${data.intValue + 1}</li>
-			<li>stringValue(字符串):${data.stringValue}</li>
-			<li>funcValue(函数):${data.funcValue()}</li>
-			<li>expression(表达式):${data.textProcess('0123456789')}</li>
-			<li>expression(表达式):${data.textProcess('0123')}</li>
-		</ul>
-	</div>
--- js/replaceAll/jsReplaceAll.js
+++ js/replaceAll/jsReplaceAll.js
@@ -1,42 +1,32 @@
 (function (win, undefined) {
-    'use strict';
-    /**
-     * String replace加强版
-     * @author yuanhongbo
-     * @createDate 2014-01-01
-     */
-    var i, l, jQuick, data, replaceCore = function (matchStr, matchVal) {
-            var ret = null;
-            try {
-                ret = eval(matchVal);
-            } catch (e) {
-                ret = matchStr;
-            }
-            return ret;
-        }, dataTemplate = function (templateStr, opt) {
-            if (!templateStr || !opt) {
-                return templateStr;
-            }
-            var datas, html = [];
-            if (opt instanceof Array) {
-                datas = opt;
-            } else {
-                datas = [];
-                datas.push(opt);
-            }
-            for (i = 0, l = datas.length; i < l; i++) {
-                data = datas[i];
-                data.index = i;
-                html.push(templateStr.replace(/\${([^}]+)}/igm, replaceCore));
-            }
-            return html.join('');
-        };
-    if (win.jQuick === undefined) {
-        jQuick = win.jQuick = {};
-    } else {
-        jQuick = win.jQuick;
-    }
-    jQuick.replaceAll = function (str, opt) {
-        return dataTemplate(str, opt);
-    };
+	'use strict';
+	var i, l, data, replaceCore = function (matchStr, matchVal) {
+			var ret = null;
+			try {
+				ret = eval(matchVal);
+			} catch (e) {
+				ret = matchStr;
+			}
+			return ret;
+		}, dataTemplate = function (templateStr, opt) {
+			if (!templateStr || !opt) {
+				return templateStr;
+			}
+			var datas, html = [];
+			if (opt instanceof Array) {
+				datas = opt;
+			} else {
+				datas = [];
+				datas.push(opt);
+			}
+			for (i = 0, l = datas.length; i < l; i++) {
+				data = datas[i];
+				data.index = i;
+				html.push(templateStr.replace(/\${([^}]+)}/igm, replaceCore));
+			}
+			return html.join('');
+		};
+	if (win.replaceAll === undefined) {
+		win.replaceAll = dataTemplate
+	};
 })(window);
