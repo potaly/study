84dadedb45579dcd7f89979f54815da927257bcb
yhb3420 <hongbo_yuan@foxmail.com>
2014-04-28 22:59:31 +0800

    JsTemplate

--- js/template/jsTemplate.js
+++ js/template/jsTemplate.js
@@ -1,70 +1,72 @@
 (function () {
 	'use strict';
-	//编译后模板缓存
-	var templateCache = {},
-
-		// 逻辑语法规则
-		LOGIC_REG = /<%([^%>]+)%>/igm;
 	/**
 	 * js模板引擎
 	 */
 	function JsTemplate(templateId, templateContent) {
-		//debugger;
 		if (!templateId) {
 			throw 'argument error.';
 		}
 		if (!(this instanceof JsTemplate)) {
 			return new JsTemplate(templateId, templateContent);
 		}
-		this.template = templateCache[templateId];
+		this.template = JsTemplate.templateCache[templateId];
 		if (!this.template) {
 			if (templateContent) {
 				this.template = JsTemplate.compile(templateContent);
-				templateCache[templateId] = this.template;
-			} else if (typeof document !== 'undefined') {
-				var tmpl = document.getElementById(templateId);
-				if (tmpl) {
-					this.template = JsTemplate.compile(tmpl.innerHTML || tmpl.value);
-					templateCache[templateId] = this.template;
-				}
+				JsTemplate.templateCache[templateId] = this.template;
+			} else {
+				throw 'template error.';
 			}
 		}
 	}
 	// 是否启用调试
 	JsTemplate.isDebug = false;
+	// 逻辑语法规则
+	JsTemplate.ANALYZE = /<%([^%>]+)%>/img;
+	//编译后模板缓存
+	JsTemplate.templateCache = {};
+	//合并属性
+	JsTemplate.mix = function (target, source) {
+		var key;
+		for (key in source) {
+			if (source.hasOwnProperty(key)) {
+				target[key] = source[key];
+			}
+		}
+		return target;
+	};
 	/**
 	 * 编译模板
 	 */
 	JsTemplate.compile = function (templateStr) {
-		var match, tmpl = templateStr,
-			lastIdx = 0,
+		var match, lastIdx = 0,
+			tmpl = templateStr,
 			funcStr = 'var $argName,$args = "",$html = "";',
 			appendFuncStr = function (content, isHtml) {
 				if (isHtml) {
-					funcStr += ('$html+="' + content.replace(/"/img, '\\"') + '";\n')
+					funcStr += ('$html+="' + content.replace(/"/img, '\\"') + '";\n');
 				} else {
 					if (content.indexOf('=') === 0) {
-						funcStr += ('$html+=' + content.substring(1) + ';\n')
+						funcStr += ('$html+=' + content.substring(1) + ';\n');
 					} else {
 						funcStr += (content.replace(/"/img, '\\"') + '\n');
 					}
 				}
 			};
 		if (JsTemplate.isDebug) {
-			funcStr += 'debugger;'
+			funcStr += 'debugger;';
 		}
-		funcStr += 'for($argName in this){$args+="var "+$argName+"=this[\'"+$argName+"\'];"} eval($args);'
+		funcStr += 'var $this = this;for($argName in $this){$args+="var "+$argName+"=$this[\'"+$argName+"\'];"} eval($args);';
+		funcStr += 'var include = function(id,includeData){return JsTemplate(id).render(JsTemplate.mix($this,includeData));};';
 		if (!tmpl) {
 			return null;
 		}
-		tmpl = tmpl.replace(/[\r\n\t]/igm, ' ');
-		while (match = LOGIC_REG.exec(tmpl)) {
-			//debugger;
-			var matchLength = match[0].length;
-			var matchIndex = match.index;
-			appendFuncStr(tmpl.substring(lastIdx === 0 ? 0 : lastIdx + 2, matchIndex), true);
+		tmpl = tmpl.replace(/[\r\n\t]/img, ' ');
+		while (match = JsTemplate.ANALYZE.exec(tmpl)) {
+			appendFuncStr(tmpl.substring(lastIdx === 0 ? 0 : lastIdx, match.index), true);
 			appendFuncStr(match[1], false);
-			lastIdx = (matchIndex + matchLength);
+			lastIdx = (match.index + match[0].length);
 		}
 		appendFuncStr(tmpl.substring(lastIdx), true);
 		appendFuncStr('return $html;', false);
